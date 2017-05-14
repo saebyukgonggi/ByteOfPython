c8b168767ebeb7c5a4d0aef8b2f5821df041fdb4
saebyukgonggi@gmail.com <root@ByeongJunui-MBP.Speedport_W723_V_Typ_A_1_01_018>
2017-05-14 21:06:22 +0200

    first commit

---
+++ str_format.py
@@ -0,0 +1,14 @@
+age = 20
+name = 'Swaroop'
+
+#print '{} was {} years old when he wrote this book'.format(name,age)
+print '{name} wrote {book}'.format(name='Swaroop',
+					book='A Byte of Python')
+print 'Why is {0} playing with that python?'.format(name)
+
+
+print "What's your name?"
+print 'What\'s your name?'
+print 'This is the first line\nThis is the second line'
+
+print r"Newlines are indicated by \n"
